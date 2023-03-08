2ac48eaa17f72285a443fc7b3b26919b639ecfc3
Shinya-SRI <127268512+Shinya-SRI@users.noreply.github.com>
2023-03-08 14:46:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
